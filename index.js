3162cdde498182520b283b14affd9f20399dc600
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2021-11-06 11:16:54 -0300

    first example with jsdom

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const axios = require("axios");
+const jsdom = require("jsdom");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const { JSDOM } = jsdom;
+
+  const { data } = await axios.get("https://quotes.toscrape.com/");
+  const dom = new JSDOM(data);
+
+  res.send(dom.window.document.querySelector(".quote").textContent);
 });
 
 app.listen(port, () => {
