069f1f3a61da1afae228a825214200a726827402
Tomas Arguelles Choo <targuelles90@gmail.com>
2021-11-06 11:32:46 -0300

    wip authors

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ app.get("/", async (req, res) => {
   res.send(dom.window.document.querySelector(".quote").textContent);
 });
 
+
+app.get("/authors", async (req, res) => {
+  const { JSDOM } = jsdom;
+  authors = []
+
+  const { data } = await axios.get("https://quotes.toscrape.com/");
+  const dom = new JSDOM(data);
+
+  
+  const ss = dom.window.document.querySelectorAll(".quote");
+  console.log(ss.length)
+  ss.forEach(element => {
+    console.log(element)
+    authors.push({
+      name: element.textContent
+    })
+    
+  });
+
+  res.send(authors);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
