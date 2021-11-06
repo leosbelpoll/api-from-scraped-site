77914cc113983d999cc06262f2ad54ea88001de2
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2021-11-06 11:40:42 -0300

    jscom util function

--- index.js
+++ index.js
@@ -5,32 +5,35 @@ const jsdom = require("jsdom");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const baseUrl = process.env.BASE_URL || "https://quotes.toscrape.com";
+
+const getDom = async (url = baseUrl) => {
+  const { data } = await axios.get(url);
+  const dom = new jsdom.JSDOM(data);
+
+  return dom.window.document;
+};
+
 app.get("/", async (req, res) => {
-  const { JSDOM } = jsdom;
+  const dom = await getDom();
 
-  const { data } = await axios.get("https://quotes.toscrape.com/");
-  const dom = new JSDOM(data);
-
-  res.send(dom.window.document.querySelector(".quote").textContent);
+  res.send(dom.querySelector(".quote").textContent);
 });
-
 
 app.get("/authors", async (req, res) => {
   const { JSDOM } = jsdom;
-  authors = []
+  authors = [];
 
   const { data } = await axios.get("https://quotes.toscrape.com/");
   const dom = new JSDOM(data);
 
-  
   const ss = dom.window.document.querySelectorAll(".quote");
-  console.log(ss.length)
-  ss.forEach(element => {
-    console.log(element)
+  console.log(ss.length);
+  ss.forEach((element) => {
+    console.log(element);
     authors.push({
-      name: element.textContent
-    })
-    
+      name: element.textContent,
+    });
   });
 
   res.send(authors);
